d43988ac7f00e40916acf6cf6d18ecef650043df
Skjamal <satinder.jamal@gmail.com>
2018-10-06 13:42:49 -0600

    FixFacebookicon

--- smoke-editor/src/components/Login.vue
+++ smoke-editor/src/components/Login.vue
@@ -26,7 +26,7 @@
           </column>
           <column is-8>
             <h1>
-             <img src=""/satinderjamal/desktop/cs3250/facebook_like_logo_1.jpg" alt="FacebookIcon" style="width:128px;height:128px;">
+             <img src="facebook_like_logo_1.jpg" alt="FacebookIcon" style="width:128px;height:128px;">
             </h1>
           </column>
         </columns>
